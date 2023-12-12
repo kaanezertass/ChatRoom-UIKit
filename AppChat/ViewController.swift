7f091d19a2fc142c7f45f456098ba1e6287a9f53
Elka Software <48525469+elkasoftwaree@users.noreply.github.com>
2023-12-12 16:40:15 +0300

    Initial Commit

---
+++ AppChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppChat
+//
+//  Created by Kaan Ezerrtaş on 12.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
